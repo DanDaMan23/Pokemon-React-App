89d1dff0ef895f2272c0eded8a2c3d82f0549ff5
DanDaMan23 <danlawrcons.gmail.com>
2022-03-06 14:44:10 -0600

    Redesign Redux Saga and Slices

--- src/components/templates/pokemonCardTemplate.js
+++ src/components/templates/pokemonCardTemplate.js
@@ -3,30 +3,13 @@ import PropTypes from "prop-types"
 import { Card } from "react-bootstrap"
 import { titleCase } from "title-case"
 
-const PokemonCardTemplate = ({ name, types, abilities }) => {
-  const { Body, Title, Subtitle } = Card
-  let pokemonTypes = ""
-  types.map((type) => (pokemonTypes += titleCase(type.type.name) + " "))
-
-  const pokemonAbilities = []
-  abilities.map((ability) =>
-    pokemonAbilities.push(titleCase(ability.ability.name))
-  )
+const PokemonCardTemplate = ({ name }) => {
+  const { Body, Title } = Card
 
   return (
     <Card>
       <Body>
         <Title>{titleCase(name)}</Title>
-        <Subtitle>Type: {pokemonTypes}</Subtitle>
-        <br />
-        <div>
-          <h5>Abilities</h5>
-          <ul>
-            {pokemonAbilities.map((ability) => (
-              <li key={ability}>{ability}</li>
-            ))}
-          </ul>
-        </div>
       </Body>
     </Card>
   )
@@ -34,8 +17,7 @@ const PokemonCardTemplate = ({ name, types, abilities }) => {
 
 PokemonCardTemplate.propTypes = {
   name: PropTypes.string.isRequired,
-  types: PropTypes.array.isRequired,
-  abilities: PropTypes.array.isRequired
+  // url: PropTypes.string.isRequired
 }
 
 export default PokemonCardTemplate
