24763df02f6cc314425c0b0503a5840f19edb365
DanDaMan23 <danlawrcons.gmail.com>
2022-03-19 12:35:56 -0500

    Footer Created

---
+++ src/components/footer/footer.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { Button, Container } from "react-bootstrap"
+
+const Footer = () => {
+  return (
+    <>
+      <div className='mt-4 mb-1 d-flex justify-content-center'>
+        <Button
+          variant='dark'
+          onClick={() => {
+            window.scrollTo(0, 0)
+          }}
+        >
+          Scroll Top
+        </Button>
+      </div>
+      <div className='bg-dark text-light p-4'>
+        <Container>
+          <h4>Pokemon React App</h4>
+        </Container>
+      </div>
+    </>
+  )
+}
+
+export default Footer
