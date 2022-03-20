652254dc4a976aa33ea8b06e780c77b2fc9c32fd
DanDaMan23 <danlawrcons.gmail.com>
2022-03-20 16:49:00 -0500

    Removed unused codes

--- src/components/forms/pokemonSearchForm.js
+++ src/components/forms/pokemonSearchForm.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Form, Button } from "react-bootstrap"
+import { Form } from "react-bootstrap"
 import { useFormik } from "formik"
 import PokemonStatsModal from "../modals/pokemonStatsModal"
 
--- src/store/store.js
+++ src/store/store.js
@@ -2,15 +2,13 @@ import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit"
 import createSagaMiddleware from "redux-saga"
 import { all } from "redux-saga/effects"
 import watchPokemonSagas from "../sagas/pokemonsSaga"
-import watchPokemonSearchSaga from "../sagas/pokemonSearchSaga"
 import watchTypesSagas from "../sagas/typesSaga"
 import spinnerSlice from "../slices/spinnerSlice"
 import pokemonsSlice from "../slices/pokemonsSlice"
-import pokemonSlice from "../slices/pokemonSlice"
 import typesSlice from "../slices/typesSlice"
 
 const rootSaga = function* () {
-  yield all([watchPokemonSagas(), watchPokemonSearchSaga(), watchTypesSagas()])
+  yield all([watchPokemonSagas(), watchTypesSagas()])
 }
 
 const sagaMiddleware = createSagaMiddleware()
@@ -19,7 +17,6 @@ const store = configureStore({
   reducer: {
     pokemons: pokemonsSlice,
     spinner: spinnerSlice,
-    pokemon: pokemonSlice,
     types: typesSlice
   },
   middleware: [...getDefaultMiddleware(), sagaMiddleware]
